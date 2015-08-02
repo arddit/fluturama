b96799157d068c400aa80ffa885fe7835f8118c9
Ardi <ardihamza@gmail.com>
2015-08-01 22:12:13 -0400

    added login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
-root 'application#index'
-get 'results' => 'application#results'
-get 'about'   => 'application#about'
-get 'contact' => 'application#contact'
-get 'help'    => 'application#help'
-get 'signup'  => 'users#new'
+get 'sessions/new'
+
+root                 'application#index'
+get    'results' => 'application#results'
+get    'about'   => 'application#about'
+get    'contact' => 'application#contact'
+get    'help'    => 'application#help'
+get    'signup'  => 'users#new'
+get    'login'   => 'sessions#new'
+post   'login'   => 'sessions#create'
+delete 'logout'  => 'sessions#destroy'
+
 resources :users
 
 end
